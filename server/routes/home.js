1c344fc7b5ad2f7d605a0a083556c77bfc80d556
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-10-27 12:16:19 +1300

    db.getAllRoles() function is now working

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,3 +1,4 @@
+import { getAllByRole } from '@testing-library/dom'
 import express from 'express'
 
 import * as db from '../db/db.js'
@@ -5,6 +6,7 @@ import * as db from '../db/db.js'
 const router = express.Router()
 
 router.get('/', async (req, res) => {
+  console.log(await db.getAllRoles())
   res.render('home')
 })
 
