8757855ac0c75253a16f83fd91616c2e01fc42ba
Blue Pilkinton-Ching <49179161+Blue-Pilkinton-Ching@users.noreply.github.com>
2023-10-27 11:57:29 +1300

    Small Changes

--- server/routes/invitation.js
+++ server/routes/invitation.js
@@ -1,11 +1,12 @@
 import express from 'express'
-
 import * as db from '../db/db.js'
 
 const router = express.Router()
 
 router.post('/', async (req, res) => {
   const data = JSON.parse(req.body)
+
+  //let jobAssignment = await db.getJobAssignment()
 
   res.send(data)
 })
